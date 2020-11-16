d25c84547b2c3b1aef33bbc526715b3697bcd6f0
glacier-4 <lsmheckle@korea.ac.kr>
2020-11-16 23:45:31 +0900

    double_major retouch
    _

--- double_major/double_major.js
+++ double_major/double_major.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-  start();
+  express_major();
+  major_detail();
 } )
 
-function start () {
-  const selectElement = document.querySelector('#major');
-    selectElement.addEventListener('change', (event) => {
+function express_major () {
+  const selectmajor = document.querySelector('#major_select');
+    selectmajor.addEventListener('change', (event) => {
       const result = document.querySelector('.result');
       result.textContent = `${event.target.value} 정보입니다.`;
+      //object Event, HTMLSelectElement
     });
   }
+
+  function major_detail() {
+    const selectmajor = document.querySelector('#major_select');
+    selectmajor.addEventListener('change', (event) => {
+      const result = document.querySelector('.result');
+      result.textContent = `${event.target.value} 정보입니다.`;
+      //object Event, HTMLSelectElement
+    });
+  }
+
+
+  //학점과 희망학과를 입력 받아 지원 안정성을 평가해주는 기능
